a5159b21193088718df17410aa106d27df468430
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-12-16 18:32:15 -0800

    Prepared the folder structure

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -4,12 +4,8 @@ import styles from "./about.style";
 
 const About = ({ info }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.headText}>About the job:</Text>
-
-      <View style={styles.contentBox}>
-        <Text style={styles.contextText}>{info}</Text>
-      </View>
+    <View>
+      <Text>About</Text>
     </View>
   );
 };
--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -5,21 +5,8 @@ import { icons } from "../../../constants";
 
 const Footer = ({ url }) => {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.likeBtn}>
-        <Image
-          source={icons.heartOutline}
-          resizeMode='contain'
-          style={styles.likeBtnImage}
-        />
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.applyBtn}
-        onPress={() => Linking.openURL(url)}
-      >
-        <Text style={styles.applyBtnText}>Apply for job</Text>
-      </TouchableOpacity>
+    <View>
+      <Text>Footer</Text>
     </View>
   );
 };
