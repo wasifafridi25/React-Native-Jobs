234ceac6dac000d15de210f3d635261e7850a929
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-12-23 19:34:14 -0800

    Loaded the custom fonts

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
+
 import {
   View,
   Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
 } from "react-native";
 
-const Welcome = () => {
+import { useRouter } from 'expo-router'
+import styles from './welcome.style'
 
+import { icons, SIZES } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter()
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Wasif</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   );
 };
--- components/home/welcome/welcome.style.js
+++ components/home/welcome/welcome.style.js
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: SIZES.medium,
     height: "100%",
+    
   },
   searchInput: {
     fontFamily: FONT.regular,
